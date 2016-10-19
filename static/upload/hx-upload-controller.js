763264660ee1f48b08cee5df3e7a3e0700f1ec5e
williamg97 <willgregory97@gmail.com>
2016-10-19 05:50:58 -0400

    .

--- static/upload/hx-upload-controller.js
+++ static/upload/hx-upload-controller.js
@@ -1,4 +1,6 @@
-﻿angular.module('hxApp').controller('HealxUploadController', ['HealxDataFactory', function (healxDataFactory) {
+﻿angular.module('hxApp').controller('HealxUploadController', ['$location', 'HealxDataFactory',
+    function ($location, healxDataFactory) {
+
     this.inputDnaSequenceText = null;
 
     this.clearDnaSequence = function () {
@@ -10,10 +12,21 @@
     }
 
     this.parse = function (newData) {
+        //$location.path('#/loading');
+        $location.path('/dnaResult');
+
         console.log('Parsing..');
 
         // use the factory to handle what to do with the data!
         healxDataFactory.uploadData(newData);
+
+        // scroll into view
+        var divPosition = $('#bottom').offset();
+        var headerSize = $('#navBar').height() + 10;
+        $('html, body').animate({ scrollTop: (divPosition.top - headerSize) }, "slow");
+
+        //$location.path('#/dnaResult');
+
     };
 }]);
 
